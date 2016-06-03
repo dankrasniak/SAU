f104daa983ab24c04582dfdc5c5a587f320b7587
Dan <dankrasniak>
2016-06-03 17:46:46 +0200

    start

--- src/com/company/MLPerceptron/CellType.java
+++
@@ -1,6 +0,0 @@
-package com.company.MLPerceptron;
-
-
-public enum CellType {
-    LINEAR, ARCTANGENT
-}
--- src/com/company/MLPerceptron/TeachingPolicies/TeachingPolicy.java
+++
@@ -1,7 +0,0 @@
-package com.company.MLPerceptron.TeachingPolicies;
-
-import com.company.MLPerceptron.Utils.Matrix;
-
-public interface TeachingPolicy {
-    Matrix Execute(final Matrix weights, final Matrix newWeights, final Matrix oldWeights);
-}
